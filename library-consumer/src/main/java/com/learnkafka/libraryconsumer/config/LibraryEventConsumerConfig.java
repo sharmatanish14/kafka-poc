20088aaf17cb7d84cc1c33da5ef4ab47d1778be3
Tanish <no email>
2024-03-02 14:45:37 +0530

    made consumer concurrent cunsumers by changing concurrency level to 3

--- library-consumer/src/main/java/com/learnkafka/libraryconsumer/config/LibraryEventConsumerConfig.java
+++ library-consumer/src/main/java/com/learnkafka/libraryconsumer/config/LibraryEventConsumerConfig.java
@@ -1,9 +1,37 @@
 package com.learnkafka.libraryconsumer.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.ContainerCustomizer;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
 @Configuration
 @EnableKafka
 public class LibraryEventConsumerConfig {
+
+    @Autowired
+    KafkaProperties properties;
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory,
+            ObjectProvider<ContainerCustomizer<Object, Object, ConcurrentMessageListenerContainer<Object, Object>>> kafkaContainerCustomizer,
+            ObjectProvider<SslBundles> sslBundles) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory.getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(
+                this.properties.buildConsumerProperties(sslBundles.getIfAvailable()))));
+        kafkaContainerCustomizer.ifAvailable(factory::setContainerCustomizer);
+        factory.setConcurrency(3);  // 3 means it will spin up three threads with the same instance of kafka listener
+        return factory;
+    }
 }
